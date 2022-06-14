4ce14b0409709823848da261191fffa22f4d3907
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2022-06-14 13:32:17 +0100

    Add files

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = get_user_model()
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return (self.title)
